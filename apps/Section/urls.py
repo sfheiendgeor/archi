63ce2699fce2dd720f76e97fe42163ab4e1c308f
SI <shingo.feeder@gmail.com>
2021-10-11 00:32:06 +0900

    fixed

--- apps/Section/urls.py
+++ apps/Section/urls.py
@@ -1,11 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import  IndexView, upload, list
-
+from .views import  IndexView, upload, list, my_customized_server_error
 urlpatterns = [
     path('', IndexView.as_view(), name = 'IndexView'),
     path('upload/', upload, name = 'upload'),
     path('list/', list.as_view(), name = 'list' ),
 ]
-handler500 = views.my_customized_server_error
+handler500 = my_customized_server_error
