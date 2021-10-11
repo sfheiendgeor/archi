85c594984c9e07dd0c7867552f0d2f75756e4c66
SI <shingo.feeder@gmail.com>
2021-10-11 23:28:56 +0900

    fixe

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.Section import views
 
+handler500 = views.my_customized_server_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
