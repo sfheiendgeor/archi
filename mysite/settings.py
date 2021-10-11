59e78fedfd7ce26216d168ae264e1fafc359a490
SI <shingo.feeder@gmail.com>
2021-10-11 22:47:35 +0900

    fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = ([os.path.join(BASE_DIR, "static")])
 
 
